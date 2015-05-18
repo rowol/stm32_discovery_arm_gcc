1322374cd260b38c4feeadb2bdd20f5626511c43
Ross Wolin <rowol@tesla.(none)>
2015-05-17 18:13:04 -0700

    Blinky: Untabify main.c
    * Formatting only, no code changes

--- blinky/main.c
+++ blinky/main.c
@@ -20,7 +20,7 @@ void ms_delay(int ms)
 int main(void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;  // enable the clock to GPIOD
-    __asm("dsb");								   // stall instruction pipeline, until instruction completes, as
+    __asm("dsb");                         // stall instruction pipeline, until instruction completes, as
                                           //    per Errata 2.1.13, "Delay after an RCC peripheral clock enabling"
     GPIOD->MODER = (1 << 26);             // set pin 13 to be general purpose output
 
